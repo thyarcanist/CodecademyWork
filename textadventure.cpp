44149d6baaa6e11f2788c70ff4c4ce4171ed4027
voidespy <48234494+voidespy@users.noreply.github.com>
2021-02-08 16:42:06 -0500

    Update textadventure.cpp
    Added branches and formatted the document better.
    
    Prompt 1A - Control Flow 2 - Going to the Window first branch is complete.
    
    TODO:
    Prompt 1A - Control Flow 1 - Going to Laptop First
    Prompt 1A - Control Flow 3 - Going to the Door First
    Prompt 2A & 3A have yet to be worked on.

--- textadventure.cpp
+++ textadventure.cpp
@@ -10,16 +10,16 @@ int main() {
 	int karma;
 	bool bIsGameOver = false;
 
-	// First Part -- Getting The Key
+	// First Part -- Getting The Key | 1a
 	bool bRetrievedKey = false;
 	bool bLuredCat = false;
 	int inputA;
 
-	// Second Part -- Finding The Man
+	// Second Part -- Finding The Man -- TO BE CONTINUED | 2a
 	bool bIsManFound = false;
 	int inputB;
 
-	// Third Part -- On The Chase
+	// Third Part -- On The Chase | 3a
 	bool bHasFoundPortal = false;
 	int inputC;
 
@@ -36,7 +36,7 @@ int main() {
 	std::cout << "3) Go to the Door.\n";
 	std::cin >> inputA;
 
-	// Control Flow - 1a
+	// Control Flow - 1a | START
 	if (inputA == 1) {
 		inputA = 0; // sets inputA back to zero
 		std::cout << "You got to the laptop and open it. You can't really read anything and it vaguely reminds you of latin.\n\n";
@@ -48,7 +48,39 @@ int main() {
 
 		// Control Flow - 1b
 		if (inputA == 1) {
-			std::cout << "You back away from the laptop. Your not sure if any good will come out of messing with it.";
+			inputA = 0;
+
+			// Description
+			std::cout << "You back away from the laptop. Your not sure if any good will come out of messing with it.\n\n";
+
+			std::cout << "What will you do?\n";
+			std::cout << "1) Go to the Window.\n";
+			std::cout << "2) Go to the Door.\n";
+			std::cin >> inputA;
+
+			// Control Flow
+			if (inputA == 1) {
+				inputA = 0;
+
+				// Description
+
+				// Control Flow
+				if (inputA == 1) {
+					std::cout << "You have gone to the window.\n";
+				}
+				else if (inputA == 2) {
+					std::cout << "You have gone to the Door.\n";
+				}
+				else {
+					std::cout << "Invalid input.\n";
+				}
+			}
+			else if (inputA == 2) {
+				std::cout << "You have gone to the door.\n";
+			}
+			else {
+				std::cout << "Invalid input.\n";
+			}
 		}
 		else if (inputA == 2) {
 			inputA = 0;
@@ -100,11 +132,54 @@ int main() {
 					// Control Flow - 1bb
 					if (inputA == 1) {
 						inputA = 0;
-						std::cout << "You try to open the door. The doorknob turns but you can't push or pull it open.\n";
+						std::cout << "You try to open the door. The doorknob turns but you can't push or pull it open.\n\n";
+
+						// Description
+						std::cout << "What will you do?\n";
+						std::cout << "1) Insert KEY in the DOOR.";
+						std::cin >> inputA;
+
+						// Control Flow - 1bc
+						if (inputA == 1) {
+							inputA = 0;
+
+							// Description
+							std::cout << "You insert the key into the door. It is stuck shut no matter how hard you turn or jiggle the doorknob.\n";
+
+							// Action
+							std::cout << "With a sigh, you take the key out of the door. There isn't much else to interact with -- and you seem to be safe in the apartment.\n";
+							std::cout << "You sit down and wait for something to happen. Anything. Sometimes you hear people step pass the door.\n";
+							std::cout << "Other times, you are looking out the window watching things go about their daily business. The cat watching with you.\n\n";
+
+							std::cout << "The door knob clicks, and then turns. The cats and your attention snap towards it, and the door creeps open.\n";
+							std::cout << "You had expected the light from the hallway to be a different color but its just a standard dim, white light.\n";
+							std::cout << "A person with horns on their hand steps in. The horns were like one that you'd see on a ram.\n";
+							std::cout << "Their eyebrow raises at the cat but nevertheless they don't say anything. They sit a package down on the counter next to the door, and then the door closes again.\n\n";
+
+							// GAME OVER
+							std::cout << "TO BE CONTINUED.\n";
+						}
+						else {
+							std::cout << "Invalid input.\n";
+						}
 					}
 					else if (inputA == 2) {
 						inputA = 0;
-						std::cout << "You insert the key into the door. It is stuck shut no matter how hard you turn or jiggle the doorknob.\n";
+						std::cout << "You insert the key into the door. It is stuck shut no matter how hard you turn or jiggle the doorknob.\n\n";
+
+						// Description
+						std::cout << "With a sigh, you take the key out of the door. There isn't much else to interact with -- and you seem to be safe in the apartment.\n";
+						std::cout << "You sit down and wait for something to happen. Anything. Sometimes you hear people step pass the door.\n";
+						std::cout << "Other times, you are looking out the window watching things go about their daily business. The cat watching with you.\n\n";
+
+						std::cout << "The door knob clicks, and then turns. The cats and your attention snap towards it, and the door creeps open.\n";
+						std::cout << "You had expected the light from the hallway to be a different color but its just a standard dim, white light.\n";
+						std::cout << "A person with horns on their hand steps in. The horns were like one that you'd see on a ram.\n";
+						std::cout << "Their eyebrow raises at the cat but nevertheless they don't say anything. They sit a package down on the counter next to the door, and then the door closes again.\n\n";
+
+						// GAME OVER
+						std::cout << "TO BE CONTINUED.\n";
+
 					}
 					else {
 						std::cout << "Invalid input.\n";
@@ -145,7 +220,92 @@ int main() {
 					// Control Flow
 
 					if (inputA == 1) {
-						std::cout << "\n";
+						inputA = 0;
+						// Description
+						std::cout << "You flicker your fingers and try to lure the cat towards you.\n";
+						std::cout << "You are able to get the cat to you with resounding success.\n";
+						std::cout << "The cat purrs in your grasp and you grab the key that was tied to its tail.\n";
+						std::cout << "You're not sure what they key can be used for...\n\n";
+
+						// Decisions
+						std::cout << "What will you do?\n\n";
+						std::cout << "1) Go to the Door";
+
+						// Control Flow - 1ba
+						if (inputA == 1) {
+							inputA = 0;
+							std::cout << "You go to the Door...\n";
+
+							std::cout << "The door is a normal one. It's color is black and the doorknob is silver in color.\n";
+							std::cout << "You would think that you could open it from the inside but you can open it, there is a keyhole that you can insert the key in. It's odd.\n\n";
+
+							// Description
+							std::cout << "What will you do?\n";
+							std::cout << "1) Try to open the door.\n";
+							std::cout << "2) Insert Key in the door.\n";
+							std::cout << "\n\n";
+
+							// Control Flow - 1bb
+							if (inputA == 1) {
+								inputA = 0;
+								std::cout << "You try to open the door. The doorknob turns but you can't push or pull it open.\n\n";
+
+								// Description
+								std::cout << "What will you do?\n";
+								std::cout << "1) Insert KEY in the DOOR.";
+								std::cin >> inputA;
+
+								// Control Flow - 1bc
+								if (inputA == 1) {
+									inputA = 0;
+
+									// Description
+									std::cout << "You insert the key into the door. It is stuck shut no matter how hard you turn or jiggle the doorknob.\n";
+
+									// Action
+									std::cout << "With a sigh, you take the key out of the door. There isn't much else to interact with -- and you seem to be safe in the apartment.\n";
+									std::cout << "You sit down and wait for something to happen. Anything. Sometimes you hear people step pass the door.\n";
+									std::cout << "Other times, you are looking out the window watching things go about their daily business. The cat watching with you.\n\n";
+
+									std::cout << "The door knob clicks, and then turns. The cats and your attention snap towards it, and the door creeps open.\n";
+									std::cout << "You had expected the light from the hallway to be a different color but its just a standard dim, white light.\n";
+									std::cout << "A person with horns on their hand steps in. The horns were like one that you'd see on a ram.\n";
+									std::cout << "Their eyebrow raises at the cat but nevertheless they don't say anything. They sit a package down on the counter next to the door, and then the door closes again.\n\n";
+
+									// GAME OVER
+									std::cout << "TO BE CONTINUED.\n";
+								}
+								else {
+									std::cout << "Invalid input.\n";
+								}
+							}
+							else if (inputA == 2) {
+								inputA = 0;
+								std::cout << "You insert the key into the door. It is stuck shut no matter how hard you turn or jiggle the doorknob.\n\n";
+
+								// Description
+								std::cout << "With a sigh, you take the key out of the door. There isn't much else to interact with -- and you seem to be safe in the apartment.\n";
+								std::cout << "You sit down and wait for something to happen. Anything. Sometimes you hear people step pass the door.\n";
+								std::cout << "Other times, you are looking out the window watching things go about their daily business. The cat watching with you.\n\n";
+
+								std::cout << "The door knob clicks, and then turns. The cats and your attention snap towards it, and the door creeps open.\n";
+								std::cout << "You had expected the light from the hallway to be a different color but its just a standard dim, white light.\n";
+								std::cout << "A person with horns on their hand steps in. The horns were like one that you'd see on a ram.\n";
+								std::cout << "Their eyebrow raises at the cat but nevertheless they don't say anything. They sit a package down on the counter next to the door, and then the door closes again.\n\n";
+
+								// GAME OVER
+								std::cout << "TO BE CONTINUED.\n";
+
+							}
+							else {
+								std::cout << "Invalid input.\n";
+							}
+						else if (inputA == 3) {
+							inputA = 0;
+							// Description
+							std::cout << "AY-YOO\n";
+						}
+						}
 					}
 					else if (inputA == 2) {
 						std::cout << "You reach out and grab the key.\n";
@@ -174,20 +334,34 @@ int main() {
 					std::cout << "Invalid input.\n";
 				}
 			}
+			else {
+				std::cout << "Invalid input.\n";
+			}
 		}
+		else if (inputA == 2) {
+			std::cout << "You go to the window. Upon peering more onto the ground, you realize that you may not be home. The figures on the ground stretch from humanoid to blob-like beings.\n";
+			std::cout << "You realize that you can open the window. There is a cat peering down on you, it's tail is flickering back and forth.\n\n";
+			std::cout << "You see the key, you open the latch and try to get it.\n\n";
+			std::cout << "You are successful!\n\n";
+			bRetrievedKey = true;
+		}
+		else if (inputA == 3) {
+			std::cout << "You go to the door and try to open it. But you fail to do so. You may need a key.\n\n";
+		}
+		else {
+			std::cout << "Invalid Input.";
+		}
+	}
 	else if (inputA == 2) {
-	std::cout << "You go to the window. Upon peering more onto the ground, you realize that you may not be home. The figures on the ground stretch from humanoid to blob-like beings.\n";
-	std::cout << "You realize that you can open the window. There is a cat peering down on you, it's tail is flickering back and forth.\n\n";
-	std::cout << "You see the key, you open the latch and try to get it.\n\n";
-	std::cout << "You are successful!\n\n";
-	bRetrievedKey = true;
+		std::cout << "You went to the window first.\n";
 	}
 	else if (inputA == 3) {
-	std::cout << "You go to the door and try to open it. But you fail to do so. You may need a key.\n\n";
+		std::cout << "You went to the door first!\n";
 	}
 	else {
-		std::cout << "Invalid Input.";
+		std::cout << "Invalid input.\n";
 	}
 
+	// END OF PROGRAM
 
-}
+	}
