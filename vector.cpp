5510b790a7d55659da32e275a1310a283f5b36f4
voidespy <48234494+voidespy@users.noreply.github.com>
2021-02-08 20:07:49 -0500

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -10,9 +10,15 @@ int main() {
   grocery.push_back("Brahmin Steak");
   grocery.push_back("StarCrunch Bar");
   grocery.push_back("Orange Mentats");
-  grocery.push_back("Nuka Cola Quantnum");
+  grocery.push_back("Nuka Cola Quantum");
 
 
+  // Prints how many items are in the list
   std::cout << grocery.size() << "\n";
+  
+  
+  std::cout << grocery[0] << "\n"; // prints Hot Pepper Jam
+  std::cout << grocery[3] << "\n"; // prints Sweet Roll
+  std::cout << grocery[7] << "\n"; // prints Nuka Cola Quantum
   
 }
